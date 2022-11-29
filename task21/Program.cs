94aebc60f00b4d5f2a4dc49a1064f4b88c328eb4
Kate <Kate-pretty@ya.ru>
2022-11-29 23:09:48 +0500

    added files

---
+++ task21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите координату x1 для точки A:");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y1 для точки A:");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1 для точки A:");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x2 для точки B:");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y2 для точки B:");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z2 для точки B:");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
